02ca3f506406dc84aa41bac81794185c4190f3af
Elvis Aguero <elvisavfc65@gmail.com>
2025-05-18 19:17:04 -0400

    added support for tuple domains (uniform)

--- python/src/surferbot/integration.py
+++ python/src/surferbot/integration.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+from collections.abc import Iterable
 
 def simpson_weights(N: int, h: float) -> jnp.ndarray:
     """
@@ -13,9 +14,14 @@ def simpson_weights(N: int, h: float) -> jnp.ndarray:
     """
     if N < 3 or N % 2 == 0:
         raise ValueError("N must be an odd integer greater than or equal to 3.")
+    if isinstance(h, jnp.ndarray):
+        # If not uniformly spaced, raise an error
+        if jnp.max(jnp.diff(h)) != jnp.min(jnp.diff(h)):
+            raise ValueError("Non uniform grids not implemented for integration.")
+        h = (h[1] - h[0])  # Use the first spacing as the uniform spacing
 
     weights = jnp.ones(N)
-    weights = weights.at[1:N-1:2].set(4)
-    weights = weights.at[2:N-2:2].set(2)
+    weights = weights.at[1:N-1:2].set(4.)
+    weights = weights.at[2:N-2:2].set(2.)
     weights = weights * (h / 3.0)
     return weights
