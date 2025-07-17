c95c3e20557cf31b97f2782b00da6b36ca788599
Elvis Whopper <elvis_vera@brown.edu>
2025-07-17 17:00:40 -0400

    updated sweeper variables

--- MATLAB/sweeper.m
+++ MATLAB/sweeper.m
@@ -6,18 +6,18 @@
 [U, x, z, phi, eta, args] = flexible_surferbot_v2( ...
     'sigma'         , 72.2e-3      , ...   % [N m?¹] surface tension
     'rho'           , 1000.0       , ...   % [kg m?³] water density
-    'omega'         , 2*pi*5      , ...   % [rad s?¹] drive frequency
+    'omega'         , 2*pi*80      , ...   % [rad s?¹] drive frequency
     'nu'            , 1.0e-6       , ...   % [m² s?¹] kinematic viscosity
     'g'             , 9.81         , ...   % [m s?²] gravity
     'L_raft'        , 0.05         , ...   % [m] raft length
-    'motor_position', 0.015        , ...   % [m] motor x-position (from -L/2 to L/2)
+    'motor_position', 1.9e-2/2     , ...   % [m] motor x-position (from -L/2 to L/2)
     'd'             , 0.03         , ...   % [m] raft depth (spanwise)
-    'EI'            , 3.0e9 * 3e-2 * (1e-4)^3 / 12 , ...  % [N m²] bending stiffness
+    'EI'            , 3.0e9 * 3e-2 * (9.9e-4)^3 / 12 , ...  % [N m²] bending stiffness
     'rho_raft'      , 0.018*3.0    , ...   % [kg m?¹] linear mass
     'L_domain'      , 0.2          , ...   % [m] domain length
     'domainDepth'   , 0.4          , ...   % [m] water depth
-    'n'             , 101          , ...   % grid points in the raft
-    'M'             , 200          , ...   % modes in z (or Fourier terms)
+    'n'             , 201          , ...   % grid points in the raft
+    'M'             , 300          , ...   % gird points in the z direction
     'motor_inertia' , 0.13e-3*2.5e-3, ...  % [kg m²] motor inertia
     'BC'            , 'radiative'        );% boundary-condition type
 
@@ -28,7 +28,8 @@ plot(x(args.x_contact), real(eta(args.x_contact)) * scale, 'r', 'LineWidth', 2)
 set(gcf, 'Position', [52 557 1632 420]);
 set(gca, 'FontSize', 16);
 xlabel('x (m)'); xlim([-.1, .1]);
-ylabel('y (um)'); %ylim([-200, 200]);
+sc = max(abs(eta), [], 'all') * 1e+6 * 1.4; scaleY = round(sc, 1, 'significant');
+ylabel('y (um)'); ylim([-scaleY, scaleY]);
 quiver(x(args.x_contact), (real(eta(args.x_contact)).') * scale, zeros(1, sum(args.x_contact)), ...
     args.loads.'/5e+4 * scale, 0, 'MaxHeadSize', 1e-6);
 
