4ddbcbec0faf55683505afa5d4bf0d3163aa73ec
Elvis Aguero <elvisavfc65@gmail.com>
2025-08-22 16:27:39 -0400

    updated sweeper test parameter

--- MATLAB/sweeper.m
+++ MATLAB/sweeper.m
@@ -5,15 +5,15 @@
 % ------------------------------------------------------------------------
 addpath './src'
 [U, x, z, phi, eta, args] = flexible_surferbot_v2( ...
-    'sigma'         , 72.2e-3      , ...   % [N m?¹] surface tension
+    'sigma'         , 0*72.2e-3      , ...   % [N m?¹] surface tension
     'rho'           , 1000.0       , ...   % [kg m?³] water density
     'omega'         , 2*pi*10      , ...   % [rad s?¹] drive frequency
-    'nu'            , 1.0e-6       , ...   % [m² s?¹] kinematic viscosity
-    'g'             , 9.81         , ...   % [m s?²] gravity
+    'nu'            , 0*1.0e-6       , ...   % [m² s?¹] kinematic viscosity
+    'g'             , 10*9.81         , ...   % [m s?²] gravity
     'L_raft'        , 0.05         , ...   % [m] raft length
     'motor_position', 1.9e-2/2   , ...   % [m] motor x-position (from -L/2 to L/2)
     'd'             , 0.03         , ...   % [m] raft depth (spanwise)
-    'EI'            , 3.0e9 * 3e-2 * (9.9e-4)^3 / 12 , ...  % [N m²] bending stiffness
+    'EI'            , 100*3.0e9 * 3e-2 * (9.9e-4)^3 / 12 , ...  % [N m²] bending stiffness
     'rho_raft'      , 0.018*3.0    , ...   % [kg m?¹] linear mass
     'L_domain'      , 0.2          , ...   % [m] domain length
     'domainDepth'   , 0.2          , ...   % [m] water depth
