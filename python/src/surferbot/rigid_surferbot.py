7ef46accb4ecdc8f42da4009cb0e475811e4d007
jennifereushim <113213802+jennifereushim@users.noreply.github.com>
2025-09-24 12:29:51 -0400

    updates to testing for rigid

--- python/src/surferbot/rigid_surferbot.py
+++ python/src/surferbot/rigid_surferbot.py
@@ -1,12 +1,11 @@
+import jax
 import jax.numpy as jnp
 from DtN import DtN_generator
-import scipy.integrate as spi
 from surferbot.myDiff import Diff
-from surferbot.utils import solve_tensor_system, gaussian_load, test_solution, dispersion_k
 from integration import simpson_weights
-import unittest
+from surferbot.constants import DEBUG
 
-def solver(rho, omega, nu, g, L_raft, L_domain, gamma, n = 100, theta, zeta):
+def rigidSolver(rho, omega, nu, g, L_raft, L_domain, gamma, x_A, F_A, n):
     '''
     Inputs: 
     - rho: density of fluid
@@ -16,24 +15,23 @@ def solver(rho, omega, nu, g, L_raft, L_domain, gamma, n = 100, theta, zeta):
     - L_domain: length of domain (m)
     - motor_inertia: intertia of motor
     - nu: kinematic viscosity
-    - gamma: surface tension
+    - gamma: surface tension (sigma is gamma in the other notation)
     - n: number of points in raft
     - N: total number of points x-direction
-    - theta, zeta: treating as givens for now
     Outputs:
     - 
     '''
 
     ## Derived dimensional Parameters (SI Units)
+    # TODO: Fix these to be dimensional? - > fix wherever you use them as well
     L_c = L_raft
     m_c = rho * L_c**2
     t_c = 1/omega
-    F_c = rho * L_c**3 / t_c**2
 
     # Equation setup
     DtN = DtN_generator(n)
     N = DtN / (L_raft / n)
-    integral = simpson_weights(H, dx)
+    integral = simpson_weights(n, dx)
 
     # Equation 1 (B)
     C11 = 1.0
@@ -43,7 +41,8 @@ def solver(rho, omega, nu, g, L_raft, L_domain, gamma, n = 100, theta, zeta):
 
     # Equation 2 (C)
     C21 = 1.0
-    C22 = -1j * (zeta + theta * x) * omega  # constant term
+    C22 = -1j
+    C23 = -1j * (x / L_c)
 
     # Equation 3 (G)
     C31 = 1.0j
@@ -52,21 +51,21 @@ def solver(rho, omega, nu, g, L_raft, L_domain, gamma, n = 100, theta, zeta):
 
     # Equation 4 (2.11b)
     C41 = -m_c
-    C42 = -F_c  # constant term
+    C42 = -F_A / (rho * L_c**3 * omega**2) # constant term
     C43 = 1.0j
     C44 = g / (omega**2 * L_c)
     C45 = 2 * nu / (omega * L_c**2)
 
     # Equation 5 (2.11c)
     C51 = -(1/12) * m_c
-    C52 = -(x / L_c) * F_c  # constant term
+    C52 = -(x_A / L_c) * (F_A / (rho * L_c**3 * omega**2)) # constant term
     C53 = 1.0j * x / L_c
     C54 = (g / (omega**2 * L_c)) * (x / L_c)
     C55 = (2 * nu / (omega * L_c**2)) * (x / L_c)
 
     # Raft Points
     L_raft_adim = L_raft / L_c
-    L_domain_adim = jnp.floor(L_domain / L_c) - jnp.floor(L_domain/L_c) % 2 + 1 # We make it odd (for simson integration)
+    L_domain_adim = jnp.floor(L_domain / L_c) - jnp.floor(L_domain/L_c) % 2 + 1 # We make it odd (for simpson integration)
     N = jnp.int32(n * L_domain_adim / L_raft_adim) 
 
     if jnp.std(x) < 1e-5:
@@ -78,21 +77,21 @@ def solver(rho, omega, nu, g, L_raft, L_domain, gamma, n = 100, theta, zeta):
     x_contact = abs(x) <= L_raft_adim/2; H = sum(x_contact)
     x_free    = abs(x) >  L_raft_adim/2; x_free = x_free.at[0].set(False); x_free = x_free.at[-1].set(False)
     assert jnp.sum(x_free) + jnp.sum(x_contact) == N-2, f"Number of free and contact points do not match the total number of points {N}." if DEBUG else None
-    left_raft_boundary = (N-H)//2; right_raft_boundary = (N+H)//2
+    left_raft_boundary = (N-H)//2
 
-    # derivative operators
+    # Derivative operators
     dx = (x[left_raft_boundary] - x[left_raft_boundary-1]).item(0)
-    d_dx = Diff(axis=0, grid=grid_x, acc=2, shape=(N, M))
+    d_dx = Diff(axis=0, grid=grid_x, acc=2, shape=(N)) # TODO: check this line
 
     x_contact = x[abs(x) <= L_c]
     x_free    = x[abs(x) >  L_c]
 
-    # Building matrix
-    # [E11][0][0][0]      [phi]
-    # [E21][0][0][0]      [eta]
-    # [E31][E32][0][0]    [zeta]
-    # [E41][E42][E43][0]  [theta]
-    # [E51][E52][0][E54]    
+    # Building matrix (Ax = b)
+    # [E11][0][0][0]      [phi]         [0]
+    # [E21][0][E23][E24]  [eta]         [0]
+    # [E31][E32][0][0]    [zeta]        [0]
+    # [E41][E42][E43][0]  [theta]       [C42]
+    # [E51][E52][0][E54]                [C52]
 
     E11 = C11 + (C12 + C14) * d_dx**2 + C13
     E12 = 0
@@ -101,15 +100,15 @@ def solver(rho, omega, nu, g, L_raft, L_domain, gamma, n = 100, theta, zeta):
 
     E21 = C21 
     E22 = 0
-    E23 = 0
-    E24 = 0
+    E23 = C22
+    E24 = C23
     
     E31 = C33
     E32 = C31 + C32 * d_dx**2
     E33 = 0
     E34 = 0
 
-    E41 = integral @ (C43 + C45 * d_dx**2) # (set endpoint?)
+    E41 = integral @ (C43 + C45 * d_dx**2)
     E42 = C44
     E43 = C41
     E44 = 0
@@ -125,13 +124,23 @@ def solver(rho, omega, nu, g, L_raft, L_domain, gamma, n = 100, theta, zeta):
     E4 = jnp.stack(E41, E42, E43, E44, axis = 1)
     E5 = jnp.stack(E51, E52, E53, E54, axis = 1)
 
-    # Concatenate (TODO: add in constant in E2, E4, E5)
-    E = jnp.stack(E1, E2, E3, E4, E5, axis = 0)
+    # Concatenate 
+    A = jnp.stack(E1, E2, E3, E4, E5, axis = 0)
+    B = jnp.stack(0, 0, 0, C42, C52, axis = 1)
 
-    # TODO: add test checking E is a square 
+    # Testing E is a square matrix
+    [r,c] = A.shape()
 
-    return A
+    solution = jax.numpy.linalg.solve(A, B)
+
+    # Splitting variables
+    phi = solution[0 : N - 1]
+    eta = solution[N : N - n - 1]
+    zeta = solution[N - n]
+    theta = solution[N - n + 1]
+
+    return (phi, eta, zeta, theta, r, c)
 
 if __name__ == "__main__":
-    solver(1, 1, 1, 1, 100, 9.8, 10, 100)
+    rigidSolver(1, 1, 1, 1, 100, 9.8, 10, 100)
 
---
+++ python/tests/test_rigidSolver.py
@@ -0,0 +1,28 @@
+import jax.numpy as jnp
+import numpy as np
+import pytest
+
+from surferbot.rigid_surferbot import rigidSolver
+
+# testing variables
+rho = 1000.
+omega = 2*jnp.pi*80.
+nu = nu = 1e-6
+g = 9.81
+L_raft = 0.05
+L_domain = 1.
+gamma = 72.2e-3
+x_A = 0.025 # check this
+f_A = 1. # check this
+n = 100
+
+# def general_test(rho, omega, nu, g, L_raft, L_domain, gamma, x_A, f_A, n):
+#     [phi, theta, eta, zeta, r, c] = rigidSolver(rho, omega, nu, g, L_raft, L_domain,gamma, x_A, f_A, n)
+    
+#     assert phi == 
+#     assert theta ==
+#     assert eta ==
+#     assert zeta == 
+def square_matrix_test(rho, omega, nu, g, L_raft, L_domain, gamma, x_A, F_A, n):
+    [phi, theta, eta, zeta, r, c] = rigidSolver(rho, omega, nu, g, L_raft, L_domain,gamma, x_A, f_A, n)
+    assert r == c
