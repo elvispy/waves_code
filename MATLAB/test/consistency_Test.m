bcf587798a28a698c2b193b2a4d49f7e25d34180
Elvis Aguero <elvisavfc65@gmail.com>
2025-11-04 16:04:42 -0500

    went back to surferbot parameters

--- MATLAB/test/consistency_Test.m
+++ MATLAB/test/consistency_Test.m
@@ -29,13 +29,10 @@ for ii = 1:numel(omega_values)
         'nu'            , 0*1.0e-6       , ...   % [m² s?¹] kinematic viscosity
         'g'             , 9.81        , ...   % [m s?²] gravity
         'L_raft'        , L_raft         , ...   % [m] raft length
-        'motor_position', 0.4*L_raft/2   , ...   % [m] motor x-position (from -L/2 to L/2)
+        'motor_position', 0.24*L_raft/2   , ...   % [m] motor x-position (from -L/2 to L/2)
         'd'             , L_raft/2            , ...   % [m] raft depth (spanwise)
-        'EI'            , 10*3.0e9 * 3e-2 * (9.9e-4)^3 / 12 , ...  % [N m²] bending stiffness
-        'rho_raft'      , 0.018*3.0    , ...   % [kg m?¹] linear mass
-        'domainDepth'   , 0.5          , ...   % [m] water depth
-        'n'             , 101          , ...   % grid points in the raft
-        'M'             , 300          , ...   % gird points in the z direction
+        'EI'            , 3.0e9 * 3e-2 * (9.9e-4)^3 / 12 , ...  % [N m²] bending stiffness
+        'rho_raft'      , 0.052    , ...   % [kg m?¹] linear mass %'domainDepth'   , 0.5          , ...   % [m] water depth %'n'             , 101          , ...   % grid points in the raft %'M'             , 300          , ...   % gird points in the z direction
         'motor_inertia' , 0.13e-3*2.5e-3, ...  % [kg m²] motor inertia
         'BC'            , 'radiative'        );% boundary-condition type
     
