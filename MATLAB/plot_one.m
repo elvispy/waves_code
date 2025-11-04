e2b40bcfbde64bccedbf91f20899f3bac578fe89
Elvis Whopper <elvis_vera@brown.edu>
2025-11-03 20:49:36 -0500

    added plot of abs(eta) and phase

--- MATLAB/plot_one.m
+++ MATLAB/plot_one.m
@@ -6,17 +6,17 @@ names = {'U', 'x', 'z', 'phi', 'eta', 'args'};
 L_raft = 0.05;
 [out{1:numel(names)}] = flexible_surferbot_v2( ...
     'sigma'         , 72.2e-3      , ...   % [N/m] surface tension
-    'rho'           , 1000.0        , ...   % [kg/m³] water density
+    'rho'           , 1000.0        , ...   % [kg/mï¿½] water density
     'omega'         , 2*pi*80        , ...   % [rad 1/s] drive frequency
-    'nu'            , 0*1.0e-6      , ...   % [m²/s] kinematic viscosity
+    'nu'            , 0*1.0e-6      , ...   % [mï¿½/s] kinematic viscosity
     'g'             , 9.81          , ...   % [m/s2] gravity
     'L_raft'        , L_raft        , ...   % [m] raft length  
     'L_domain'      , 5*L_raft      , ...
     'motor_position', 0.24*L_raft/2  , ...   % [m] motor x-position (from -L/2 to L/2)
     'd'             , 0.03         , ...   % [m] raft depth (spanwise)
-    'EI'            , 3.0e9 * 3e-2 * (9.9e-4)^3 / 12 , ...  % [N m²] bending stiffness
+    'EI'            , 3.0e9 * 3e-2 * (9.9e-4)^3 / 12 , ...  % [N mï¿½] bending stiffness
     'rho_raft'      , 0.052        , ...   % [kg/m] linear mass %'loads'         , linspace(-1, 1, 101), ... %'domainDepth'   , 10*L_raft     , ...   % [m] water depth % %'n'             , 101          , ...   % grid points in the raft %'M'             , 200          , ...   % gird points in the z direction
-    'motor_inertia' , 0.13e-3*2.5e-3, ...  % [kg m²] motor inertia
+    'motor_inertia' , 0.13e-3*2.5e-3, ...  % [kg mï¿½] motor inertia
     'BC'            , 'radiative'        );% boundary-condition type
 
 
